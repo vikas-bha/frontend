1c338fc17435cf037f7d5c066d62cefd41124009
Vikas Bhardwaj <vikasbhardwaj@Vikass-Air.Dlink>
2024-01-26 15:10:33 +0530

    removing singup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from 'react'
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [isSignUp, setIsSignUp] = useState(true);
 
     useEffect(()=>{
         console.log("signup got called")
@@ -12,7 +11,7 @@ const Signup = () => {
     const handleSubmit = (e) => {
       e.preventDefault();
       // call that signup/ login api here
-      console.log(`Email: ${email}, Password: ${password}, Mode: ${isSignUp ? 'Sign Up' : 'Login'}`);
+      console.log(`Email: ${email}, Password: ${password}`);
     };
   
   
@@ -23,7 +22,8 @@ const Signup = () => {
     
       <div className="flex items-center justify-center h-screen">
         <form className="bg-white p-8 rounded shadow-md" onSubmit={handleSubmit}>
-          <h2 className="text-2xl font-semibold mb-4">{isSignUp ? 'Sign Up' : 'Login'}</h2>
+          <h2 className="text-2xl font-semibold mb-4">{'Login'}</h2>
+
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
             <input
@@ -50,18 +50,9 @@ const Signup = () => {
             type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline-blue"
           >
-            {isSignUp ? 'Sign Up' : 'Login'}
+            Login
           </button>
-          <p className="mt-4">
-            {isSignUp ? 'Already have an account? ' : 'Don\'t have an account? '}
-            <button
-              type="button"
-              className="text-blue-500 underline focus:outline-none"
-              onClick={() => setIsSignUp(!isSignUp)}
-            >
-              {isSignUp ? 'Login' : 'Sign Up'}
-            </button>
-          </p>
+        
         </form>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Signup from './components/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
